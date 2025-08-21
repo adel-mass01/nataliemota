9dc6fad3a1fcfbb8e5536ba3e86b3c816544924a
massouab adel <adel-massouab@outlook.com>
2025-08-21 21:11:42 +0200

    rm: ancien contact-modal (dossier templates_part)

--- templates_part/contact-modal.php
+++
@@ -1,7 +0,0 @@
-<div class="contact-modal" id="contactModal">
-  <div class="modal-overlay" id="modalOverlay"></div>
-  <div class="modal-content">
-    <?php echo do_shortcode('[fluentform id="3"]'); ?>
-  </div>
-</div>
-
