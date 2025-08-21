0b4460660bfbd2c12de3eb028e25dedd62e02841
massouab adel <adel-massouab@outlook.com>
2025-08-21 21:18:31 +0200

    creation du single-photo

---
+++ single-photo.php
@@ -0,0 +1,17 @@
+<?php 
+
+get_header(); ?> 
+
+
+<main id="primary" class="site-main">
+
+
+<?php while ( have_posts()) : the_post();
+
+get_template_part('template-parts/content', 'single-photo');
+
+endwhile; ?>
+
+</main>
+
+<?php get_footer(); ?>
